6a62359df84f10fe3fada2a6ceb18e9f9ccd171b
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-10-25 23:24:32 -0300

    Criada a função produto

--- core/views.py
+++ core/views.py
@@ -1,21 +1,24 @@
 from multiprocessing import context
 from django.shortcuts import render
+from core.models import Produto
 
 def index(request):
-    print(dir(request))
-    print(f'User: {request.user}')
-
-    if str(request.user) == 'AnonymousUser':
-        teste = 'Usuário não logado!'
-    else:
-        teste = 'Usuário logado!'
-
+    produtos = Produto.objects.all()
+    
     context = {
         'curso': 'Programação web com Django Framework',
         'outro': 'Django é massa!',
-        'logado': teste
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
